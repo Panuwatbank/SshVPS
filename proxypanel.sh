0a0ea37769f33ff56e94178c80fb68e4b2953cbb
Panuwatbank <35716517+Panuwatbank@users.noreply.github.com>
2018-05-05 03:04:14 +0700

    Update proxypanel.sh

--- proxypanel.sh
+++ proxypanel.sh
@@ -11,14 +11,14 @@ tput setaf 2 ; tput bold ; printf '%s' "|1|"; tput setaf 6 ; printf '%s' " Disab
 tput setaf 2 ; tput bold ; printf '%s' "|2|"; tput setaf 6 ; printf '%s' " Enable" ; tput setaf 4 ; printf '%s' " = " ; tput setaf 7 ; echo "Habilitara a autenticacao por Proxy" ; tput sgr0 ;
 tput setaf 2 ; tput bold ; printf '%s' "|0|"; tput setaf 6 ; printf '%s' " Sair" ; tput setaf 4 ; printf '%s' " = " ; tput setaf 7 ; echo "Simplesmente voltara para o menu" ; tput sgr0 ;
 echo ""
-tput setaf 7 ; tput setab 4 ; tput bold ; printf '%30s%s%-10s\n' "Digite a opcao desejada" ; tput sgr0 ; echo ""
+tput setaf 7 ; tput setab 4 ; tput bold ; printf '%30s%s%-10s\n' "จอมปลอม" ; tput sgr0 ; echo ""
 read  opcao
 
 case $opcao in
 	1) limiteron ;;
 	2) limiteroff ;;
-	3) reinstall ;;
-	0) exit ;;
+	3) ติดตั้ง ;;
+	0) ทางออก ;;
 esac
 
 }
@@ -54,25 +54,14 @@ cache deny all\n
 http_access deny all\n
 http_port 80\n
 http_port 3128\n
+http_port 8080\n
+http_port 8000\n
+
 visible_hostname VIP.ไอ้เหี้ยไอ้สัสเย็ดเป็ดเย็ดไก่(WY.ม้าอาชา)\n
 forwarded_for off\nvia off\n" > squid.conf
 service squid3 restart
 clear
-tput setaf 7 ; tput setab 4 ; tput bold ; printf '%30s%s%-10s\n' "Proxy Auth OFFILINE" ; tput sgr0 ; echo ""
-tput setaf 7 ; tput setab 4 ; tput bold ; printf '%30s%s%-10s\n' "Nao e mais necessario autenticacao proxy" ; tput sgr0 ; echo ""
-}
-
-limiteroff()
-{
-clear
-IP=$(curl https://api.ipify.org/)
-echo $IP
-cd /etc/squid*
-mv squid.conf squid.confbak
-mv squid.confauth squid.conf
-service squid3 restart
-clear
-tput setaf 7 ; tput setab 4 ; tput bold ; printf '%30s%s%-10s\n' "Proxy Auth Online" ; tput sgr0 ; echo ""
+tput setaf 7 ; tput setab 4 ; tput bold ; printf '%30s%s%-10s\n' "พร็อกซี่ Auth Online" ; tput sgr0 ; echo ""
 tput setaf 7 ; tput setab 4 ; tput bold ; printf '%30s%s%-10s\n' "Necessario autenticacao PROXY" ; tput sgr0 ; echo ""
 }
 
